537cfd19f8688e573b0100466087e25ef9b46b26
steven-gardiner <steven_gardiner@yahoo.com>
2015-03-11 11:16:08 -0400

    additional dest folder

--- node/xbrw/download_sources.js
+++ node/xbrw/download_sources.js
@@ -3,26 +3,27 @@ xbrwdl.mods = {};
 xbrwdl.mods.fs = require('fs');
 xbrwdl.mods.cp = require('child_process');
 
-xbrwdl.download_sources = function(manfile) {
+xbrwdl.download_sources = function(manfile, dest) {
   var spec = {};
   spec.json = JSON.parse(xbrwdl.mods.fs.readFileSync(manfile, 'utf8'));
 
-  spec.parent = manfile.split(/\//).slice(0,-1).join("/");  
+  spec.parent = manfile.split(/\//).slice(0,-1).join("/");
+  spec.dest = dest || spec.parent;  
   console.error("PAR: %s", spec.parent);
   
   console.error("JSON: %j", spec.json.sources);
 
   for (key in spec.json.sources) {
-    var curl = xbrwdl.mods.cp.spawn('curl', [
+    var curl = remanifest.mods.cp.spawn('curl', [
 					     '-o', key,
 					     '-z', key,
 					     spec.json.sources[key]
 					     ],
-				    {cwd: spec.parent});
+				    {cwd: spec.dest});
 
     curl.stdout.pipe(process.stdout);
     curl.stderr.pipe(process.stderr);
   }
 };
 
-xbrwdl.download_sources(process.argv[2]);
+xbrwdl.download_sources(process.argv[2], process.argv[3]);
