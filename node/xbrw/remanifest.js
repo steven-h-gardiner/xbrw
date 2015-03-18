9379699f7f5faf8946d42eb0f3d877d324492914
steven-gardiner <steven_gardiner@yahoo.com>
2015-03-17 21:37:12 -0400

    also copy unsourced resources

--- node/xbrw/remanifest.js
+++ node/xbrw/remanifest.js
@@ -15,23 +15,32 @@ console.error("ARGS: %j", remanifest.args);
 
 remanifest.manifest = JSON.parse(remanifest.mods.fs.readFileSync(remanifest.args.infile));
 
-for (filename in remanifest.manifest.sources) {
-  var inurl = remanifest.manifest.sources[filename];
+remanifest.rewrite = function(inurl) {
+  switch (inurl) {
+    case "inject-html.js":
+    case "jquery-noconflict.js":
+      return inurl;
+  }
 
-  var outfile = remanifest.mods.path.resolve(remanifest.args.outdir, filename);
+    
+  var outfile = remanifest.mods.path.resolve(remanifest.args.outdir, inurl);
   
   var purl = remanifest.mods.url.parse(inurl);  
   var outurl = function(urlobj, url) {
     console.error("PROTOCOL: " + urlobj.protocol);
     if (! urlobj.protocol) {
-      var filepath = url;
-      var absolute = remanifest.mods.path.resolve(filepath);      
-      var newrel = remanifest.mods.path.relative(remanifest.args.outdir, absolute);
-      var local = remanifest.mods.path.basename(filepath);
+      var info = {};
+      info.filepath = url;
+      info.absolute = remanifest.mods.path.resolve(info.filepath);      
+      info.newrel = remanifest.mods.path.relative(remanifest.args.outdir, info.absolute);
+      info.local = remanifest.mods.path.basename(info.filepath);
+      info.outfile = outfile;
+      
+      console.error("FILEINFO %j", info);
+      
+      remanifest.mods.fs.createReadStream(info.absolute).pipe(remanifest.mods.fs.createWriteStream(outfile));
 
-      remanifest.mods.fs.createReadStream(absolute).pipe(remanifest.mods.fs.createWriteStream(outfile));
-
-      return local;
+      return info.local;
     }
 
     var wget = remanifest.mods.cp.spawn('wget', ['-N',
@@ -45,7 +54,26 @@ for (filename in remanifest.manifest.sources) {
     
     return remanifest.mods.url.format(urlobj);
   }(purl, inurl);
-  remanifest.manifest.sources[filename] = outurl;  
+  return outurl;
+};
+
+remanifest.maybeRewrite = function(url) {
+  if (remanifest.manifest.sources[url]) { return url; }
+  return remanifest.rewrite(url);
+};
+
+console.error("SOURCES");
+for (filename in remanifest.manifest.sources) {
+  remanifest.manifest.sources[filename] = remanifest.rewrite(remanifest.manifest.sources[filename]);
 }
+
+remanifest.manifest.content_scripts.forEach(function(script) {
+    console.error("JS");
+  script.js = script.js.map(remanifest.maybeRewrite);
+  console.error("CSS");
+  script.css = script.css.map(remanifest.maybeRewrite);
+  console.error("HTML");
+  script.html = script.html.map(remanifest.maybeRewrite);
+});
 
 remanifest.mods.fs.writeFileSync(remanifest.args.outfile, JSON.stringify(remanifest.manifest, null, 2));
